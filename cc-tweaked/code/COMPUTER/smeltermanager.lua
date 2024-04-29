6cb4f4aba36410b9e7786a91219ac9dbebde5f8a
Tyler Bauer <tbauer516@users.noreply.github.com>
2024-04-28 22:18:34 -0700

    blast furnace needs more code to filter ores

--- cc-tweaked/code/COMPUTER/smeltermanager.lua
+++ cc-tweaked/code/COMPUTER/smeltermanager.lua
@@ -374,9 +374,9 @@ m.processInventory = function(self, mappedInventory)
     local success = self:smeltFromQueue(item, itemAmount, fuel, fuelAmount, furnace)
     if (success) then
       local timeAmount = 10
-      if (string.find(peripheral.getName(furnace), "blast_furnace")) then
-        timeAmount = 5
-      end
+      -- if (string.find(peripheral.getName(furnace), "blast_furnace")) then
+      --   timeAmount = 5
+      -- end
       self.furnaceStack[#self.furnaceStack] = nil
       os.queueEvent("timer_furnaceloaded", (timeAmount * itemAmount) + 0.5, furnace.index)
     else
